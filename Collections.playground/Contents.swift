d4c442e408eb048ff5d70ccde7983d10336e2b8e
paperabyss <103468074+paperabyss@users.noreply.github.com>
2024-02-02 08:09:23 +0900

    Challenge 38 completed

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -28,3 +28,16 @@ assert([5,15,55,515].challenge37(count: 5) == 6, "Challenge 37 failed.")
 assert([5,15,55,515].challenge37(count: 1) == 2, "Challenge 37 failed.")
 assert([55555].challenge37(count: 5) == 5, "Challenge 37 failed.")
 assert([55555].challenge37(count: 1) == 0, "Challenge 37 failed.")
+
+
+
+extension Collection where Iterator.Element: Comparable{
+    func challenge38(count: Int) -> [Iterator.Element] {
+        return Array(self.sorted().prefix(count))
+    }
+}
+
+assert([1,2,3,4].challenge38(count: 3) == [1,2,3],"Challenge 38 failed")
+assert(["q", "f", "k"].challenge38(count: 10) == ["f", "k", "q"], "Challenge 38 failed")
+assert([256,16].challenge38(count: 3) == [16,256], "Challenge 38 failed")
+assert([String]().challenge38(count: 3) == [], "Challenge 38 failed")
