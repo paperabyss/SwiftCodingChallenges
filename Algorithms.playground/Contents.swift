86394fee8eac03a1797123a0d297a50b330860f2
paperabyss <103468074+paperabyss@users.noreply.github.com>
2024-02-09 08:31:01 +0900

    Completed challenge 56.

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -17,7 +17,6 @@ extension Array where Element: Comparable {
                 } else if i < array[index-1] {
                     array.swapAt(index, index-1)
                     swappedItems += 1
-                    print(array)
                 }
             }
             if swappedItems == 0 {
@@ -31,3 +30,28 @@ extension Array where Element: Comparable {
 assert([12, 5, 4, 9, 3, 2, 1].challenge55() == [1,2,3,4,5,9,12], "Challenge 55 failed.")
 assert(["f","a", "b"].challenge55() == ["a","b","f"], "Challenge 55 failed.")
 assert([String]().challenge55() == [], "Challenge 55 failed.")
+
+extension Array where Element: Comparable {
+    func challenge56() -> [Element] {
+        guard !self.isEmpty else { return [] }
+        var sortedArray: [Element] = []
+
+        for i in self {
+            if sortedArray.isEmpty {
+                sortedArray.insert(i, at: 0)
+            } else {
+                for (index, j) in sortedArray.enumerated() {
+                    if i < j {
+                        sortedArray.insert(i, at: index)
+                        break
+                    }
+                }
+            }
+        }
+        return sortedArray
+    }
+}
+
+assert([12, 5, 4, 9, 3, 2, 1].challenge56() == [1,2,3,4,5,9,12], "Challenge 56 failed.")
+assert(["f","a", "b"].challenge56() == ["a","b","f"], "Challenge 56 failed.")
+assert([String]().challenge56() == [], "Challenge 56 failed.")
